b66c86c57d533932cbbcb77ab8c43214a315f420
GitJohnDrop <juan.heouairi@gmail.com>
2018-02-27 10:43:08 +0100

    handcheck

---
+++ Hand Check.ps1
@@ -0,0 +1,6 @@
+$nombre=20
+$total =0
+for ($ser=$nombre-1;$ser>0;$ser--){
+$total=$total+$ser
+}
+$total
---
+++ hello.ps1
@@ -0,0 +1,8 @@
+for($i=0;$i -lt 20;$i++){
+Write-Host $i
+}
+
+do{
+Write-Host $i
+}
+while ($i -lt 20)
